9badc3a1350a4047674126a5cf90df943ab6ca19
Mario Meissner <mr.rash.boy@gmail.com>
2018-11-16 21:00:35 +0100

    cosas

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+
+var fs = require('fs');
+var server = http.createServer(function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+        res.write(data);
+        res.end();
+    });
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
