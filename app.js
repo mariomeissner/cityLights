3a23b6c01f9abd204eb15137da00e4b00ba044c6
Mario Meissner <mr.rash.boy@gmail.com>
2018-11-17 05:51:11 +0100

    funcion parcial

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+// Cosas para DB
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+// Create connection to database
+var config =
+{
+    userName: 'antonio', // update me
+    password: 'asd123..', // update me
+    server: 'prueba1111.database.windows.net', // update me
+    options:
+    {
+        database: 'prueba1111' //update me
+        , encrypt: true
+    }
+}
+
+// Cosas para el servidor
 const http = require('http');
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 var path = require('path');
 var express = require('express');
@@ -11,16 +28,16 @@ var dir = path.join(__dirname, 'public')
 app.use(express.static(dir));
 
 var Storage = multer.diskStorage({
-    destination: function(req, file, callback) {
+    destination: function (req, file, callback) {
         callback(null, "./Images");
     },
-    filename: function(req, file, callback) {
+    filename: function (req, file, callback) {
         callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
     }
 });
 
-app.post("/api/Upload", function(req, res) {
-    upload(req, res, function(err) {
+app.post("/api/Upload", function (req, res) {
+    upload(req, res, function (err) {
         if (err) {
             return res.end("Something went wrong!");
         }
@@ -28,20 +45,48 @@ app.post("/api/Upload", function(req, res) {
     });
 });
 
+app.get('/street_info', function (req, res) {
+    return queryDatabase(req)
+})
+
 var upload = multer({
     storage: Storage
 }).array("imgUploader", 3);
 
-app.listen(port,() => {
-console.log(`Server running at port `+port);
+app.listen(port, () => {
+    console.log(`Server running at port ` + port);
 });
 
+function queryDatabase() {
 
-// var fs = require('fs');
-// var server = http.createServer(function (req, res) {
-//     fs.readFile('public/index.html', function (err, data) {
-//         res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
-//         res.write(data);
-//         res.end();
-//     });
-// });
+    var connection = new Connection(config);
+
+    // Attempt to connect and execute queries if connection goes through
+    connection.on('connect', function (err) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            {
+                console.log('Reading rows from the Table...');
+
+                // Read all rows from table
+                request = new Request(
+                    "SELECT * from dbo.Calle",
+                    function (err, rowCount, rows) {
+                        console.log(rowCount + ' row(s) returned');
+                        process.exit();
+                    }
+                );
+
+                request.on('row', function (columns) {
+                    columns.forEach(function (column) {
+                        console.log("%s\t%s", column.metadata.colName, column.value);
+                    });
+                });
+                connection.execSql(request);
+            }
+        }
+    }
+    );
+}
