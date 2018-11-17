f5f55d63449aca1fd3b3cc71f4c221cd8d124822
Mario Meissner <mr.rash.boy@gmail.com>
2018-11-17 04:18:28 +0100

    add support for file uploading

--- app.js
+++ app.js
@@ -3,9 +3,38 @@ const port=process.env.PORT || 3000
 
 var path = require('path');
 var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
 var dir = path.join(__dirname, 'public')
 app.use(express.static(dir));
+
+var Storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+app.post("/api/Upload", function(req, res) {
+    upload(req, res, function(err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+});
+
+var upload = multer({
+    storage: Storage
+}).array("imgUploader", 3);
+
+app.listen(port,() => {
+console.log(`Server running at port `+port);
+});
 
 
 // var fs = require('fs');
@@ -16,6 +45,3 @@ app.use(express.static(dir));
 //         res.end();
 //     });
 // });
-app.listen(port,() => {
-console.log(`Server running at port `+port);
-});
